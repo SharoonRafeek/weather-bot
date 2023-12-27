504cec9ec20f019f1a4f4749bef49117704cee22
SharoonRafeek <sharoonrafeek@gmail.com>
2023-12-27 11:41:50 +0530

    Update weather update schedule

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -142,7 +142,7 @@ export class TelegramService implements OnModuleInit {
   }
 
   private scheduleWeatherUpdates() {
-    cron.schedule('* * * * *', async () => {
+    cron.schedule('45 6 * * *', async () => {
       try {
         const users = await this.userModel.find();
         for (const user of users) {
