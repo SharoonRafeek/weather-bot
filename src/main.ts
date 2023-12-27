26e332c04e5e6249f6b51e9194b4487db6f4d5eb
SharoonRafeek <sharoonrafeek@gmail.com>
2023-12-27 11:07:12 +0530

    Update app.listen() to use the value of process.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   });
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
