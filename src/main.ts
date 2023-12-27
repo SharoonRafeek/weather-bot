bf141fe14abee5c2f7e139c7df79a8efe14ee9e8
SharoonRafeek <sharoonrafeek@gmail.com>
2023-12-27 10:54:27 +0530

    Update CORS origin in main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   });
   await app.listen(process.env.PORT || 8080);
 }
